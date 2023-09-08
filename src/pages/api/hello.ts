e2406f7d2ada1876a3760e9275cc1692bf76043a
Bilal Gokdag <bilalgokdag.tt@gmail.com>
2023-09-08 16:48:35 +0300

    Initialized starter kit. - Husky added. - Prettier & eslint added.

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // eslint-disable-next-line no-magic-numbers
+  res.status(200).json({ name: "John Doe" });
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div>Hello World!</div>
+    </main>
+  );
+}
